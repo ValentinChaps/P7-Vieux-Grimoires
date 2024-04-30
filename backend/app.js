9cbfed9458cc341e5c469caeb12a9aa9a08b11c9
Valentin Chapel <109554149+ValentinChaps@users.noreply.github.com>
2024-05-01 00:21:46 +0200

    Mise en place du backend

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book');
+
+
+mongoose.connect('mongodb+srv://jeanjudas:JeanPotter@cluster0.qw9ihu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/books' , bookRoutes)
+app.use('/api/auth' , userRoutes)
+
+
+module.exports = app;
